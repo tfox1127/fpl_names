cef9b4b9732ac3aa5dafc68f20b24d58acd95d92
tfox1127 <65046510+tfox1127@users.noreply.github.com>
2020-09-13 20:31:33 -0500

    idk

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-#from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+#from flask_session import Session
 from send_mail import send_mail
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -8,13 +9,29 @@ app = Flask(__name__)
 
 ENV = 'dev'
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 engine = create_engine('postgres://kbhpppsbtsabyk:6f9f47eb4721c77c17f1fccefeb2693a629e1f6e571bad88143561ba10e422be@ec2-52-20-248-222.compute-1.amazonaws.com:5432/d22l4qure274m')
-db = scoped_session(sessionmaker(bind=engine))
-SQLALCHEMY_POOL_RECYCLE = 60
+                                                  # DATABASE_URL is an environment variable that indicates where the database lives
+db = scoped_session(sessionmaker(bind=engine))    # create a 'scoped session' that ensures different users' interactions with the
+                                                  # database are kept separate
+
+
+#engine = create_engine('postgres://kbhpppsbtsabyk:6f9f47eb4721c77c17f1fccefeb2693a629e1f6e571bad88143561ba10e422be@ec2-52-20-248-222.compute-1.amazonaws.com:5432/d22l4qure274m')
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
+#db = scoped_session(sessionmaker(bind=engine))
+#ter system set idle_in_transaction_session_timeout='1min';
+#SET SESSION idle_in_transaction_session_timeout = '1min';
+#SQLALCHEMY_POOL_RECYCLE = 60
+#alter database dbnamehere set statement_timeout = 600;
 
 @app.route('/')
 def index():
-    elements = db.execute("SELECT * FROM blog ORDER BY post_number DESC LIMIT 10").fetchall()
+    elements = db.execute('SELECT * FROM blog LIMIT 10')
+    db.commit()
     return render_template('index.html', elements=elements)
 
 @app.route('/lms')
@@ -31,8 +48,10 @@ def cups():
 
 @app.route('/live')
 def live():
-    elements = db.execute('SELECT * FROM live').fetchall()
+    elements = db.execute("SELECT * FROM live")
+    db.commit()
     return render_template('live.html', elements=elements)
+
 
 @app.route('/leaderboard')
 def leaderboard():
