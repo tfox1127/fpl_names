35651ba9d9eaf0a1e84acecdb49b89da1210f2d9
tFox <tfox1127@gmail.com>
2021-10-15 08:31:51 -0500

    Week 8 picks

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ engine = create_engine(DATABASE_URL, isolation_level="AUTOCOMMIT")
 db = scoped_session(sessionmaker(bind=engine))
 app.secret_key = 'pizza'
 
-CURRENT_WEEK = 7
+CURRENT_WEEK = 8
 
 @app.route('/')
 def index():
