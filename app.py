a80b766c8dd622fb01880309d6f557c833ad5896
tAir <tommyfox@hey.com>
2021-10-21 20:28:58 -0500

    forgot equals

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.secret_key = 'pizza'
 #CURRENT_WEEK = 9
 #CURRENT_WEEK, FIRST_UNFINISHED_WEEK = api_check.pull_current_week()
 CURRENT_WEEK = 9 
-FIRST_UNFINISHED_WEEK 9 
+FIRST_UNFINISHED_WEEK = 9 
 
 @app.route('/')
 def index():
